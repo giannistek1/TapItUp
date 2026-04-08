34eb5fddb03052839b3697e86337c09a692a8e00
giannistek <gianni.stekkinger@hotmail.nl>
2026-04-08 15:11:09 +0200

    Sentry debugging added

--- TapItUp/MauiProgram.cs
+++ TapItUp/MauiProgram.cs
@@ -12,6 +12,19 @@ namespace TapItUp
                 .UseMauiApp<App>()
                 .UseMauiCommunityToolkit()
                 .UseMauiCommunityToolkitMediaElement()
+                .UseSentry(options =>
+                {
+                    // The DSN is the only required setting.
+                    options.Dsn = "https://cb6327228f262128b3c3bf9190e6e919@o4507512238637056.ingest.de.sentry.io/4511184527097936";
+
+                    // Use debug mode if you want to see what the SDK is doing.
+                    // Debug messages are written to stdout with Console.Writeline,
+                    // and are viewable in your IDE's debug console or with 'adb logcat', etc.
+                    // This option is not recommended when deploying your application.
+                    options.Debug = true;
+
+                    // Other Sentry options can be set here.
+                })
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
